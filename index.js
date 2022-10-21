5b2c71da418b05b36e1cdd5731790a15f1e758e2
RashmiBansode <rashmibansode86@gmail.com>
2022-10-21 15:57:01 +0530

    commited

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+let a = prompt('Enter an Integer No. to check it is Even or Odd');
+
+if (a % 2 == 0) {
+    document.write(a+ '  is even no');
+} else { document.write(a+  '  is odd no'); }
+
+
